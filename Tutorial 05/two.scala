ede5eb8c7308188540941bd451b7c708d5baa1ef
Tharu0 <87253039+Tharu0@users.noreply.github.com>
2022-08-22 22:52:43 +0530

    Question 2

---
+++ Tutorial 05/two.scala
@@ -0,0 +1,22 @@
+import scala.io.StdIn.readInt
+object Q2 extends App{
+
+    def primeNo(num:Int,n:Int=2):Int= n match{
+        case x if(n==num)=>num
+        case x if(num%n==0)=>0
+        case x if(num%n!=0)=>primeNo(num,n+1)
+    }
+
+    def primeSeq(N:Int):Unit={
+        if(N>0){
+            primeSeq(N-1);
+            if(primeNo(N)!=0){
+                println(N);
+            }
+        }
+    }
+
+    printf("Enter number: ");
+    var N=readInt();
+    primeSeq(N-1);
+}
