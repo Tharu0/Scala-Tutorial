82b0f3ba434bac75e9def11631d22389ef796fcb
Tharu0 <87253039+Tharu0@users.noreply.github.com>
2022-08-23 07:00:39 +0530

    Question 01

--- Tutorial 05/1.scala
+++ Tutorial 05/1.scala
@@ -2,6 +2,7 @@ import scala.io.StdIn.readInt
 object Q1 extends App{
 
     def primeNo(num:Int,n:Int=2):Boolean= n match{
+        case x if(num<=1)=>false
         case x if(n==num)=>true
         case x if(num%n==0)=>false
         case x if(num%n!=0)=>primeNo(num,n+1)
